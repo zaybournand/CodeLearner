cc11f650280838dd2c1a38922c192f26026c1c68
zaybournand <zaybournand@gmail.com>
2025-10-30 22:44:17 -0400

    Initial Setup

---
+++ backend/src/main/java/com/learn2code/backend/Learn2codeApplication.java
@@ -0,0 +1,14 @@
+package com.learn2code.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learn2codeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Learn2codeApplication.class, args);
+
+    }
+
+}
---
+++ backend/src/test/java/com/learn2code/backend/Learn2codeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn2code.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Learn2codeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
