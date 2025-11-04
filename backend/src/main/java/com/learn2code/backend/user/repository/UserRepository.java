c5fba1e952fefb39d6aaf426c3b7c65a8e44c694
zaybournand <zaybournand@gmail.com>
2025-11-03 20:19:06 -0500

    Added the Repostiory for User

--- backend/src/main/java/com/learn2code/backend/user/repository/UserRepository.java
+++ backend/src/main/java/com/learn2code/backend/user/repository/UserRepository.java
@@ -1,1 +1,17 @@
 package com.learn2code.backend.user.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.learn2code.backend.user.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+}
